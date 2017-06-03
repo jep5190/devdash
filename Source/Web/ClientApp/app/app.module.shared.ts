3336fbebbac5597da3d7a4b84129292d1c732068
Yerko Nauls <piolo108@hotmail.com>
2017-06-02 23:53:03 -0400

    added placeholder components for dashboard and configure

--- Source/Web/ClientApp/app/app.module.shared.ts
+++ Source/Web/ClientApp/app/app.module.shared.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './components/app/app.component'
+import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ConfigureComponent } from './components/configure/configure.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
@@ -14,14 +16,18 @@ export const sharedConfig: NgModule = {
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        ConfigureComponent,
+        DashboardComponent
     ],
     imports: [
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'dashboard', component: DashboardComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'configure', component: ConfigureComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ Source/Web/ClientApp/app/components/configure/configure.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'configure',
+    template: require('./configure.component.html')
+})
+export class ConfigureComponent {
+}
